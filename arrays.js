7c6b30559bc2c5ec6e6363753fea5d33a2658953
Matt Doering <mattdoering86@gmail.com>
2018-10-19 07:28:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array , element) {
-  var newArr = [...array, element];
+  var newArr = [element, ...array];
   return newArr;
 }
 function destructivelyAddElementToBeginningOfArray(array , element) {
